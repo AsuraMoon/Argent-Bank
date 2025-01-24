b81fb86be88cc229807a4339de0a8e8f50a260da
AsuraMoon <jufredenucci@gmx.fr>
2025-01-24 10:08:59 +0100

    Modification call API atlas

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3001/api/v1/user'
+export const API_URL = 'https://argent-bank-api-2wrb.onrender.com/api/v1/user'
 
 export const URL_LOGIN = API_URL + '/login'
 export const URL_SINGUP = API_URL + '/signup'
